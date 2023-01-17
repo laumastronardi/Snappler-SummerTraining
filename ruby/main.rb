51e282ed2dc4bfa895a9ecc1be617e7816ace4ea
Lautaro Mastronardi <lautaromastronardi@gmail.com>
2023-01-17 19:40:58 -0300

    Arreglos main

--- ruby/main.rb
+++ ruby/main.rb
@@ -12,18 +12,20 @@ require 'test'
 require 'sinatra'
 
 db = Main.new('./gem/lib/swapi.json')
+people = Person.new(db)
+vehicles = Vehicles.new(db)
+starships = Starships.new(db)
+
+puts people.find_by_name('C3PO')
 
 get '/people/:id' do
-  people = Person.new(db)
   puts people.find_by_name(params[:id])
 end
 
 get '/vehicles/:id' do
-  vehicles = Vehicles.new(db)
   puts vehicles.find_by_name(params[:id])
 end
 
 get '/starships/:id' do
-  starships = Starships.new(db)
   puts starships.find_by_name(params[:id])
 end
