290a39378b361426d2fdcfbaf69f38cc59703ea0
Lautaro Mastronardi <lautaromastronardi@gmail.com>
2023-01-27 13:44:18 -0300

    I18n y boton para cambiar de idioma agregado, faltan algunas traducciones

--- SummerApp/app/controllers/application_controller.rb
+++ SummerApp/app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  I18n.default_locale = :es
+
   def current_user
     @current_user ||= User.first
   end
-  
+
+  def change_lang
+    I18n.default_locale = if I18n.default_locale == :es
+                            :en
+                          else
+                            :es
+                          end
+    redirect_to root_path
+  end
 end
--- SummerApp/config/routes.rb
+++ SummerApp/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :users do
     post 'follow', to: 'users#toggle_follow'
   end
+
+  patch 'lang', to: 'application#change_lang'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'users#index'
